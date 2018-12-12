26d43303486396f8d4bd90bfb4b49fff0d693919
NJUTto <161220120@smail.nju.edu.cn>
2018-12-12 19:56:59 +0800

    changetest

--- avare-master/app/src/test/java/com/ds/avare/test/HelperTest1.java
+++ avare-master/app/src/test/java/com/ds/avare/test/HelperTest1.java
@@ -25,12 +25,4 @@ public class HelperTest1 {
     public void isSameGPSLocation2() throws Exception {
         assertEquals(true, example.isSameGPSLocation(118.6, 32.6, 118.5, 32.6));
     }
-    @Test
-    public void isSameGPSLocation3() throws Exception {
-        assertEquals(true, example.isSameGPSLocation(118.52, 32.6, 118.5, 32.6));
-    }
-    /*@Test
-    public void isSameGPSLocation() throws Exception {
-        assertEquals(true,Helper.isSameGPSLocation(118.5,32.6,118.5,32.6));
-    }*/
 }
